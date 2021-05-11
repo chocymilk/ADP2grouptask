be5efc01a1726583357758c314e8a51636e59c04
Alizandre <80397057+Alizandre@users.noreply.github.com>
2021-05-11 15:09:06 +0200

    Add files via upload

---
+++ ADPAssignment/ADP.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adp219328099;
+
+/**
+ *
+ * @author iyatiti
+ */
+public class ADP {
+    public String play(){
+        return"playing games";
+    }
+    
+}
---
+++ ADPAssignment/ADPTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adp219328099;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author iyatiti
+ */
+public class ADPTest {
+    
+    public ADPTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of play method, of class ADP.
+     */
+    @org.junit.Test
+    public void testPlay() {
+        System.out.println("play");
+        ADP instance = new ADP();
+        String expResult = "";
+        String result = instance.play();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
